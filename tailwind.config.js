e527de38b1bb285d10e5b0a79b81b5b60aac4021
Xavier MALPARTY <xavier@nimblehq.co>
2022-09-04 14:02:03 +0700

    [#5] Apply keyboard mapping to the pads

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./app/**/*.{html,js}'],
+  content: ['./app/**/*.{html,js}', './app/styles/**/*.css'],
   theme: {
     extend: {}
   },
