031c432043868f16753277a198b68077b4547368
MarkG <markgravity.in@gmail.com>
2022-09-20 14:01:33 +0700

    [#10] Support play instrument sound

--- app/javascript/sound-engines/sampler-engine.js
+++ app/javascript/sound-engines/sampler-engine.js
@@ -1,13 +1,25 @@
+import InstrumentService from "../services/instrument.service";
+
 class SamplerEngine {
   constructor() {
     this.name = 'Sampler';
     this.instrument = '-';
+    InstrumentService.loadSample('assets/sample.wav')
+      .then(sample => {
+          this.synthService = new InstrumentService(sample)
+      })
+
   }
 
   onPadPressed(padIndex) {
     console.log(`Pad ${padIndex} pushed from the Sampler engine`);
 
     // Implement here the logic to produce Sound based on the Pad Index (int)
+    // TODO: Play in correct synth mode only
+    if (this.synthService) {
+        this.synthService.play(padIndex / 15)
+    }
+
   }
 
   changeInstrument() {
